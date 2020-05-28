28db523c8e7afc4b574c2cc6d2fddbb24b8488e3
Bader Alsabah <bhyss@hypertext.com.kw>
2020-05-28 22:36:31 +0300

    created react context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom'; 
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -21,10 +23,12 @@ function App() {
     <React.Fragment>
       
       <Navbar/>
-      <ProductList> </ProductList>
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route path="/details" component={Details}/>
+        <Route path="/cart" component={Cart}/> 
+        <Route component={Default}/>
+      </Switch>
       
     </React.Fragment>
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,39 @@
 import React, {Component} from "react";
 import Product from "./Product";
+import Title from "./Title"
+import {storeProducts} from "../data";
+import {ProductConsumer} from "../context";
 
 export default class ProductList extends Component {
+
+    state = {
+        products : storeProducts
+    }
 
     render () {
 
         return (
-            <div>
-
-                <Product/>
-
+            <>
+            <div className="py5">
+                <div className="container">
+								<Title name='our ' title='products'/>
+                    <div className="row">
+											<ProductConsumer>
+												{ (value)=> {
+													return (
+													value.products.map((item) => { 
+													console.log(item.id);	
+													return (<Product key={item.id}/>)})
+													)
+													}
+												}
+											</ProductConsumer>
+                    </div>
+                </div>
             </div>
+                {/* <Product/> */}
+
+            </>
             )
 
 
