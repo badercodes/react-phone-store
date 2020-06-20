26c8267a9b81568fe3b79574a277663ed4488d85
Bader Alsabah <bhyss@hypertext.com.kw>
2020-06-20 22:17:32 +0300

    started working on details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ProductConsumer} from "../context";
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Details</h3>
-            </div>
+          <ProductConsumer>
+            { (value) => {
+                const {id, title, img, price, company, info, inCart} = value.details;
+               console.log(company);
+              }
+            }
+
+
+          </ProductConsumer> 
         )
     }
 }
